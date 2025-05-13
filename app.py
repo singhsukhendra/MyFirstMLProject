830847d8c287421ec1e1bc77c482f40179403849
Sukhendra Singh <singhsukhendra@users.noreply.github.com>
2025-05-13 14:44:54 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def generate_house_data(n_samples=100):
   np.random.seed(50)
   size=np.random.normal(1400,500,n_samples)
   price=size*50+np.random.normal(0,500,n_samples)
-  return pd.DataFrame({'size:size,'price':prixe})
+  return pd.DataFrame({'size':size,'price':price})
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -35,7 +35,7 @@ def main():
 
   size=st.number_input("House size", min_value=500, max_value=2000,value=1500)
 
-  if st.utton("Pridict price"):
+  if st.button("Pridict price"):
     predicted_price=model.predict([[size]])
     st.success(f'Estimated price: ${predicted_price[0]:,.2f}')
 
@@ -49,3 +49,5 @@ def main():
 
 if __name__=="__main__":
   main()
+
+
